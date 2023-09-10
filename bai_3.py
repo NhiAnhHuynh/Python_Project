9e5a8d9d703b144f030f2460b9b75261cb753983
NhiAnhHuynh <nhianhhuynh230311@gmail.com>
2023-09-10 09:38:46 +0700

    first commit

---
+++ bai_3.py
@@ -0,0 +1,17 @@
+print("Xin chào, đây là máy tính tự động")
+name=(input("Vui lòng nhập tên của bạn "))
+print("Xin chào," , name)
+print(" Rare Menu :))):")
+Blood = 10000000
+Heart = 9000000000
+Lung = 7000000000
+EinsteinBrain = 300000000000
+print("1. Blood: 10 000 000VND/giọt")
+print("------------------------------------------------------------------")
+print("2. Heart: 9 000 000 000VND/trái")
+print("------------------------------------------------------------------")
+print("3. Lung: 7 000 000 000VND/lá")
+print("------------------------------------------------------------------")
+print("4. EinsteinBrain: 300 000 000 000 000VND/cái => This is limited ITEM")
+print("------------------------------------------------------------------")
+order=(input("Vui lòng nhập order của bạn: "))
